79dac8fd0c374041ffeae045df0a0e942e48c464
arturogs <arturogsuarez@gmail.com>
2017-06-04 19:10:31 -0300

    chequeo de estado en pedidospendientes

--- TP_Servidor/src/dao/AdministracionDAO.java
+++ TP_Servidor/src/dao/AdministracionDAO.java
@@ -221,7 +221,7 @@ public class AdministracionDAO {
 			Session session = sf.openSession();
 			@SuppressWarnings("unchecked")
 //			List<PedidoEntity> lista = session.createQuery("from PedidoEntity where estado='En Verificacion'").list();
-			List<PedidoEntity> lista = session.createQuery("from PedidoEntity").list();
+			List<PedidoEntity> lista = session.createQuery("from PedidoEntity p where p.estado = 'PENDIENTE'").list();
 			session.close();
 			List<Pedido> pedidos = new ArrayList<Pedido>();
 			for (PedidoEntity pedidoEntity : lista) {
