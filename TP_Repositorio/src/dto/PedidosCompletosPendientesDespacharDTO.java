d298d3fdfbb332b02fcf226193f13e8c6eff5bec
Jonathan Medina <jona.medina@gmail.com>
2017-06-19 16:56:50 -0300

    se agrega dto para mostrar pedidos pendientes de despachar

---
+++ TP_Repositorio/src/dto/PedidosCompletosPendientesDespacharDTO.java
@@ -0,0 +1,53 @@
+package dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PedidosCompletosPendientesDespacharDTO  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Date fechaCreacion;
+	private Date fechaProbableDespacho;
+	private Integer idSucursal;
+	private Integer idCliente;
+	private String nombreCliente;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public Date getFechaProbableDespacho() {
+		return fechaProbableDespacho;
+	}
+	public void setFechaProbableDespacho(Date fechaProbableDespacho) {
+		this.fechaProbableDespacho = fechaProbableDespacho;
+	}
+	public Integer getIdSucursal() {
+		return idSucursal;
+	}
+	public void setIdSucursal(Integer idSucursal) {
+		this.idSucursal = idSucursal;
+	}
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+		
+}
